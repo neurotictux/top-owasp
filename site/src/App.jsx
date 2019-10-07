7685042256f0f10596ca58a7bc5887f6ab896ce9
Gabriel Santos <gabrielsantoscimol@gmail.com>
2019-10-06 23:44:45 -0300

    Sidebar with menu
    Signed-off-by: Gabriel Santos <gabrielsantoscimol@gmail.com>

--- site/src/App.jsx
+++ site/src/App.jsx
@@ -1,16 +1,24 @@
 import React from 'react'
 import { Provider } from 'react-redux'
+import { HashRouter, Route } from 'react-router-dom'
 
-import { Home } from './components/Home/Home'
-import { Toolbar } from './components/Toolbar/Index'
+import { Sidebar, Toolbar } from './components'
+import { Home, XssReflected, XssStored } from './scene'
 import Store from './store'
-import { AppContainer } from './components/styles'
+import { AppContainer, CoreAppContainer } from './components/styles'
 
 export default () => (
   <Provider store={Store}>
     <AppContainer>
       <Toolbar />
-      <Home />
+      <CoreAppContainer>
+        <HashRouter>
+          <Sidebar />
+          <Route path="/" exact={true} component={Home} />
+          <Route path="/xss-reflected" exact={true} component={XssReflected} />
+          <Route path="/xss-stored" exact={true} component={XssStored} />
+        </HashRouter>
+      </CoreAppContainer>
     </AppContainer>
   </Provider>
 )
---
+++ site/src/scene/Home/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import { Container } from './styles'
+
+export function Home() {
+  return (
+    <Container>
+      <h4>Home</h4>
+    </Container>
+  )
+}
