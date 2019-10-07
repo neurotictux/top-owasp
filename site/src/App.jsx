3c89cb46972ca957d1d5c612a4bf124267b5f0d0
Gabriel Santos da Rosa <gabrielsantoscimol@gmail.com>
2019-10-07 12:04:47 -0300

    Create react routes and components

--- site/src/App.jsx
+++ site/src/App.jsx
@@ -3,9 +3,23 @@ import { Provider } from 'react-redux'
 import { HashRouter, Route } from 'react-router-dom'
 
 import { Sidebar, Toolbar } from './components'
-import { Home, XssReflected, XssStored } from './scene'
 import Store from './store'
-import { AppContainer, CoreAppContainer } from './components/styles'
+import { AppContainer, CoreAppContainer, BaseContainer } from './components/styles'
+import { ROUTES } from './utils'
+
+import {
+  BruteForce,
+  Home,
+  XssReflected,
+  XssStored,
+  CommandInjection,
+  Csrf,
+  FileIncusion,
+  FileUpload,
+  Login,
+  SessionHijaking,
+  SqlInjection
+} from './scene'
 
 export default () => (
   <Provider store={Store}>
@@ -14,9 +28,19 @@ export default () => (
       <CoreAppContainer>
         <HashRouter>
           <Sidebar />
-          <Route path="/" exact={true} component={Home} />
-          <Route path="/xss-reflected" exact={true} component={XssReflected} />
-          <Route path="/xss-stored" exact={true} component={XssStored} />
+          <BaseContainer>
+            <Route path={ROUTES.BRUTE_FORCE} exact={true} component={BruteForce} />
+            <Route path={ROUTES.COMMAND_INJECTION} exact={true} component={CommandInjection} />
+            <Route path={ROUTES.CSRF} exact={true} component={Csrf} />
+            <Route path={ROUTES.FILE_INCLUSION} exact={true} component={FileIncusion} />
+            <Route path={ROUTES.FILE_UPLOAD} exact={true} component={FileUpload} />
+            <Route path={ROUTES.HOME} exact={true} component={Home} />
+            <Route path={ROUTES.LOGIN} exact={true} component={Login} />
+            <Route path={ROUTES.SESSION_HIJAKING} exact={true} component={SessionHijaking} />
+            <Route path={ROUTES.SQL_INJECTION} exact={true} component={SqlInjection} />
+            <Route path={ROUTES.XSS_REFLECTED} exact={true} component={XssReflected} />
+            <Route path={ROUTES.XSS_STORED} exact={true} component={XssStored} />
+          </BaseContainer>
         </HashRouter>
       </CoreAppContainer>
     </AppContainer>
--- site/src/scene/Home/Home.jsx
+++ site/src/scene/Home/Home.jsx
@@ -1,11 +1,5 @@
 import React from 'react'
 
-import { Container } from './styles'
-
 export function Home() {
-  return (
-    <Container>
-      <h4>Home</h4>
-    </Container>
-  )
+  return <h4>Home</h4>
 }
